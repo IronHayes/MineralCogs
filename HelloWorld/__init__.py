bacf7b16087670434ce0d14268443a5ff3175fba
Tristan Klett <kletttristan@gmail.com>
2024-05-28 12:47:27 -0500

    Removed HelloWorld
    Removed HelloWorld cog to fix naming convention

--- HelloWorld/__init__.py
+++
@@ -1,4 +0,0 @@
-from .helloworld import HelloWorld
-
-async def setup(bot):
-    await bot.add_cog(HelloWorld(bot))
--- HelloWorld/helloworld.py
+++
@@ -1,9 +0,0 @@
-from redbot.core import commands
-
-class HelloWorld(commands.Cog):
-    """A testing cog to test redcore and github repos"""
-
-    @commands.command()
-    async def test(self, ctx):
-        """Returns 'Hello World!' in the channel"""
-        await ctx.send("Hello World!")
